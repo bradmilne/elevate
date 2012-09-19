a99bd5fd4ab5cad45ae5eefb5a0b847cef399d46
Brad Milne <brad.milne@getelevate.com>
2012-09-19 11:46:03 -0400

    elevate website start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elevate::Application.config.secret_token = 'e4ec332c48b1a6c5615829791f86ceb27d989a4f45bed8ca3593213c0f96caa18275dd8c039ef1e7531ade5bcc537fe35491aa9b9d1546f75c3cc26d769eccae'
